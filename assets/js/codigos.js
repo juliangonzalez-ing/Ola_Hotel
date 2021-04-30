364e6ddb76b46ceb571db9165a428ffcc95b0b68
juliangonzalez-ing <edgar.gonzalez123@outlook.com>
2021-04-29 20:23:10 -0500

    eliminando contenido de codigos.js que no se utilizaban

--- assets/js/codigos.js
+++ assets/js/codigos.js
@@ -1,33 +1,4 @@
-/*     var num1 = prompt("Ingrese el primer número: ");
-    var num2 = prompt("Ingrese el segundo número: ");
-    var suma = parseInt(num1) + parseInt(num2);
-    alert("La suma de los números ingresados es: " + suma)
-    var contador = 1;
-    var sumaLista = 1;
-    while(contador <= suma){
-        console.log(sumaLista);
-        contador++;
-        sumaLista = sumaLista + contador;        
-    }
- */
-/* 
-    var num1 = prompt("ingrese un número: ");
-    if((num1 % 2) == 0){
-        console.log("El número es par");
-    }else{
-        console.log("El número es impar");
-    }
- *//* 
-   var num1 = prompt("ingrese un número: ");
-   var num2 = prompt("Cantidad de multiplicaciones: ");
-   if(num1 != '' && num2 != ''){
-       for(var x = 1; x <= num2; x++){                       
-           alert(num1 +" * " + x + " = " + (num1*x))                    
-       }
-   }else{        
-       alert('Ingrese todos los datos');
-   }
-   */
+
 $('#carouselExampleSlidesOnly').carousel({
     interval: 5000,
 });
@@ -62,16 +33,7 @@ function darInformacion() {
     }
 }
 
-function select() {
-    var opc = document.getElementById("menu").value
 
-    let precio = 0;
-    if (opc == "papas") {
-        precio = 1000;
-        document.getElementById('hid').value = precio
-    }
-    console.log(precio)
-}
 function informacion(){}
     
 
